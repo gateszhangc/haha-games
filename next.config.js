55eaca29ef45b456332b801b017addcdfa86d2c2
gateszhangc <gateszhang92@gmail.com>
2025-11-27 10:55:34 +0800

    提交

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        // 为所有资源添加 CORS 头
+        source: '/assets/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET' },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
